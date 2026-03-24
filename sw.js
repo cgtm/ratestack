9f36fe00017059e2c25d39c747df2071035b15df
cgtm <cgtm@users.noreply.github.com>
2026-03-24 12:23:20 +0000

    deploy: 61371c77342286839fc586d027be9096613f977d

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ratestack-309f7a2';
+const CACHE_NAME = 'ratestack-61371c7';
 const STATIC_ASSETS = [
   './', './index.html', './style.css', './manifest.json',
   './src/app.js', './src/api.js', './src/state.js', './src/currencies.js',
